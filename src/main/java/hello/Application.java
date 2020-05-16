9203031eb04658c9a62724e62a71b4ac212c835e
dramajo2 <1_d.ramajo@alumnos.upm.es>
2020-05-16 22:12:05 +0200

    make 2 the application executable issue #2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,7 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+//hola
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
